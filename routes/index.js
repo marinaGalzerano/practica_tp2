3500b585454c9133688e6ab5be771bc2e60ec571
MarinaGalzerano <marinagalzerano98@gmail.com>
2024-11-30 18:06:53 -0300

    practica tp2

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /**
  * NO MODIFICAR AQUI
  */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 /**
  * MODIFICAR AQUI (Consigna 1)
  */
-router.get('/texts', function(req, res, next) {
+router.get("/texts", function (req, res, next) {
   /**
    * La funcion render devuelve el formulario de la carpeta utils/formulario.ejs
    * Aqui solo debes enviar los errores cuando haya
    */
-  res.render('formulario', { errors: [] });
+  res.render("formulario", { errors: [] });
+});
+
+router.post("/texts", function (req, res, next) {
+  /**
+   * La funcion render devuelve el formulario de la carpeta utils/formulario.ejs
+   * Aqui solo debes enviar los errores cuando haya
+   */
+  res.render("formulario", { errors: [] });
 });
 
 module.exports = router;
